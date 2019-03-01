0af193bbdea3a2a471bd600a1d8115e154953144
QAP12 <QAP12@QAP12-PC>
2019-03-01 11:56:32 +0530

    commit456

--- src/allu1/raki123.java
+++ src/allu1/raki123.java
@@ -1,5 +1,34 @@
 package allu1;
 
-public class raki123 {
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+public class raki123{
+	@BeforeClass
+	public static void login()
+	{
+		System.out.println("Logging In...");
+	}
+	@AfterClass
+	public static void logout()
+	{
+		System.out.println("Logging out...");
+	}
+	@Test
+	public void method3()
+	{
+		
+		Assert.assertEquals(11, 11);
+		System.out.println("Method3");
+	}
+	@Test
+	public void method4()
+	{
+		Assert.assertEquals(11, 11);
+		System.out.println("Method4");
+	}
 }
+
+
